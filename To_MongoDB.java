e1e84422f104c13180041008a6b6cb55d1b56fc7
IMSteno <80048218+IMSteno@users.noreply.github.com>
2021-05-31 17:37:49 +0200

    Update To_MongoDB.java

--- To_MongoDB.java
+++ To_MongoDB.java
@@ -7,6 +7,7 @@ import static com.mongodb.client.model.Updates.currentDate;
 import org.bson.Document;
 import org.bson.types.ObjectId;
 
+import javax.print.Doc;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -37,7 +38,18 @@ public class To_MongoDB{
 
     public List<Document> getUserBookings(String user_id){
 
-        return  bookings_collection.find(new Document("Booking_User_ID", user_id)).into(new ArrayList<>());
+        return bookings_collection.find(new Document("Booking_User_ID", user_id)).into(new ArrayList<>());
+    }
+
+    public List<Document> getAssociatedBookings(List <String> bookings_ids){
+
+        List<Document> all_bookings = new ArrayList<Document>();
+
+        for(String id : bookings_ids){
+            Document temp = (Document) (bookings_collection.find(eq("_id",id)));
+            all_bookings.add(temp);
+        }
+        return all_bookings;
     }
 
     public List<Document> getUserReports(String user_id){
@@ -61,7 +73,7 @@ public class To_MongoDB{
         Document temp2 = (Document) vehicles_status_collection.find(and(eq("Vehicle_ID", v_id), eq("Date", date_formatter.format(today)), or(eq("Current_Stop", entry), eq("Current_Stop", exit))));
         ObjectId d_id = temp2.getObjectId("Current_Driver_ID");
 
-        reports_collection.insertOne(new Document("ReportType",ReportType).append("Date", date_formatter.format(today)).append("Time_Reported", date_formatter.format(now)).append("Description", Description).append("Related_Booking_ID", booking_id).append("Verification_Status", 0).append("Verification_Date", null).append("Reporting_User_ID", reporting_user_id).append("Vehicle_ID", v_id).append("Driver_Validating_ID", d_id));
+        reports_collection.insertOne(new Document("ReportType",ReportType).append("Date", date_formatter.format(today)).append("Time_Reported", date_formatter.format(now)).append("Description", Description).append("Driver_Comment", null).append("Related_Booking_ID", booking_id).append("Verification_Status", 0).append("Verification_Date", null).append("Reporting_User_ID", reporting_user_id).append("Vehicle_ID", v_id).append("Driver_Validating_ID", d_id));
     }
 
     public List<Document> showUnconfirmedReports(String driver_id){
